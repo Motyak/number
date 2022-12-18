9331b8f62788386295a06a93a3602f442dd646aa
Tommy Anfosso <anfossotommy@hotmail.fr>
2022-12-18 03:39:22 +0100

    error handling

--- run_tests.sh
+++ run_tests.sh
@@ -1,35 +1,29 @@
 #!/usr/bin/env bash
 
-# set -o errexit
-# set -o errtrace
-# set -o nounset
-# set -o pipefail
+set -o errexit
+set -o errtrace
+set -o nounset
+set -o pipefail
 
-# trap handle_err ERR
-# trap cleanup SIGINT SIGTERM EXIT
-
+trap handle_errexit ERR
+trap clean_and_exit SIGINT SIGTERM EXIT
 
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
-
-
 function require {
-    declare -r E_MISSING_ARGS=11
-    declare -r E_ASSERT_FAILED=12
-
-    [ $# -ge 2 ] || exit $E_MISSING_ARGS
+    declare -r E_REQUIREMENT_NOT_MET=10
 
     read -a caller_output <<< "$(caller 0)"
-    lineno=${caller_output[0]}
-    func_name=${caller_output[1]}
-    filename=${caller_output[2]}
+    local lineno=${caller_output[0]}
+    local func_name=${caller_output[1]}
+    local filename=${caller_output[2]}
 
     local condition=$*
 
     $condition || {
         >&2 echo "Assertion \`$condition\` failed in $func_name"
         >&2 echo "  -> $filename:$lineno"
-        exit $E_ASSERT_FAILED
+        clean_and_exit $E_REQUIREMENT_NOT_MET
     }
 }
 
@@ -50,9 +44,11 @@ for key in "${!path[@]}"; do declare -r "$key"="$(abspath "${path[$key]}")"; don
 unset path
 
 function assert_src_exists {
+    declare -r E_NO_SRC_FILE=20
+
     [ -f "$SRC_FILE" ] || {
         >&2 echo "$SRC_FILE cannot be found"
-        exit 1
+        clean_and_exit $E_NO_SRC_FILE
     }
 }
 
@@ -72,23 +68,39 @@ function build_binary {
 
 function run_binary {
     require [ -f "$BINARY_FILE" ]
+    mkdir bin # for testing purpose
 
-    "$BINARY_FILE"
+    exec "$BINARY_FILE"
 }
 
-# function cleanup {
-#     trap - SIGINT SIGTERM EXIT
-#     echo "cleanup"
-# }
+function handle_errexit {
+    declare -r E_COMMAND_FAILING=11
 
-# function handle_err {
-#     trap - ERR
-#     echo "handle_err"
-# }
+    local status=$?
+    local cmd=$BASH_COMMAND
+    local func_name=${FUNCNAME[1]:-main context}
+    local filename=${BASH_SOURCE[1]}
+    local lineno=${BASH_LINENO[0]}
 
-# trap 'echo "ERR trap called in ${FUNCNAME-main context}."' ERR
+    trap - ERR
+
+    >&2 echo "Command \`$cmd\` failed in $func_name with status $status"
+    >&2 echo "  -> $filename:$lineno"
+
+    clean_and_exit $E_COMMAND_FAILING
+}
+
+
+function clean_and_exit {
+    local exit_code=${1:-0}
+
+    trap - SIGINT SIGTERM EXIT
+
+    # clean tmp resources, ..
+
+    exit $exit_code
+}
 
 assert_src_exists
 
-{ binary_exists_and_is_up_to_date || build_binary; } \
-&& run_binary
+{ binary_exists_and_is_up_to_date || build_binary; } && run_binary
