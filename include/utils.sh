4669459fcbf8d8fd38f364f899a969e4920b6908
Motyak <m0tyak_@live.fr>
2022-12-18 16:33:23 +0100

    better structure

---
+++ include/utils.sh
@@ -0,0 +1,55 @@
+function require {
+    declare -r E_REQUIREMENT_NOT_MET=98
+
+    read -a caller_output <<< "$(caller 0)"
+    local lineno=${caller_output[0]}
+    local func_name=${caller_output[1]}
+    local filename=${caller_output[2]}
+
+    local condition=$*
+
+    $condition || {
+        >&2 echo "Assertion \`$condition\` failed in $func_name"
+        >&2 echo "  -> $filename:$lineno"
+        exit $E_REQUIREMENT_NOT_MET
+    }
+}
+
+function handle_errexit {
+    local status=$?
+
+    trap - ERR
+
+    declare -r E_COMMAND_FAILING=99
+
+    local cmd=$BASH_COMMAND
+    local func_name=${FUNCNAME[1]:-main context}
+    local filename=${BASH_SOURCE[1]}
+    local lineno=${BASH_LINENO[0]}
+
+    >&2 echo "Command \`$cmd\` failed in $func_name with status $status"
+    >&2 echo "  -> $filename:$lineno"
+
+    exit $E_COMMAND_FAILING
+}
+
+function handle_sigint  {
+    declare -r E_SIGINT_SENT=130 # 128 + 2 (SIGINT)
+
+    trap - SIGINT
+
+    >&2 echo # newline after ^C
+    >&2 echo "SIGINT was sent"
+
+    exit $E_SIGINT_SENT
+}
+
+function handle_sigterm {
+    declare -r E_SIGTERM_SENT=143 # 128 + 15 (SIGTERM)
+
+    trap - SIGTERM
+
+    >&2 echo "SIGTERM was sent"
+
+    exit $E_SIGTERM_SENT
+}
--- run_tests.sh
+++ run_tests.sh
@@ -12,22 +12,8 @@ trap clean_and_exit EXIT
 
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
-function require {
-    declare -r E_REQUIREMENT_NOT_MET=98
-
-    read -a caller_output <<< "$(caller 0)"
-    local lineno=${caller_output[0]}
-    local func_name=${caller_output[1]}
-    local filename=${caller_output[2]}
-
-    local condition=$*
-
-    $condition || {
-        >&2 echo "Assertion \`$condition\` failed in $func_name"
-        >&2 echo "  -> $filename:$lineno"
-        exit $E_REQUIREMENT_NOT_MET
-    }
-}
+# import { require, handle_errexit, handle_sigint, handle_sigterm }
+source "$SCRIPT_DIR/include/utils.sh"
 
 function abspath {
     local relpath="$1"
@@ -74,45 +60,6 @@ function run_binary {
     # echo $$; read # for testing purpose
 
     exec "$BINARY_FILE"
-}
-
-function handle_errexit {
-    local status=$?
-
-    trap - ERR
-
-    declare -r E_COMMAND_FAILING=99
-
-    local cmd=$BASH_COMMAND
-    local func_name=${FUNCNAME[1]:-main context}
-    local filename=${BASH_SOURCE[1]}
-    local lineno=${BASH_LINENO[0]}
-
-    >&2 echo "Command \`$cmd\` failed in $func_name with status $status"
-    >&2 echo "  -> $filename:$lineno"
-
-    exit $E_COMMAND_FAILING
-}
-
-function handle_sigint  {
-    declare -r E_SIGINT_SENT=130 # 128 + 2 (SIGINT)
-
-    trap - SIGINT
-
-    >&2 echo # newline after ^C
-    >&2 echo "SIGINT was sent"
-
-    exit $E_SIGINT_SENT
-}
-
-function handle_sigterm {
-    declare -r E_SIGTERM_SENT=143 # 128 + 15 (SIGTERM)
-
-    trap - SIGTERM
-
-    >&2 echo "SIGTERM was sent"
-
-    exit $E_SIGTERM_SENT
 }
 
 function clean_and_exit {
