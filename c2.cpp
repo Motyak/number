7f59a209aa0b3cb3dcb13f7d672979b0945eb4b4
Motyak <m0tyak_@live.fr>
2022-12-23 18:44:27 +0100

    new spec

---
+++ c2.cpp
@@ -0,0 +1,27 @@
+
+int main()
+{
+    Number x = "197 802";
+    Number y = "263 736";
+    Number z = "395 604";
+
+    for (Number nb : {x, y, z}) {
+        sum(nb[_3]) & sum(nb[_2]) & period(9);
+
+        nb <<= 2;
+        sum(nb[_3]) & sum(nb[_2]) & period(9);
+
+        nb <<= 2;
+        sum(nb[_3]) & sum(nb[_2]) & period(9);
+    }
+
+    // x +/ 3 == y;
+    (x + x/3) == y;
+    (x + 34percent2) == y;
+
+    // y +/ 2 == z:
+    (y + y/2) == z;
+    (y + 51percent2) == z;
+
+    (x * 2) = z;
+}
